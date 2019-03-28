922b8cff40ba7cdbb4cd60fcadd4840c618f684d
Jay778 <949219778@qq.com>
2019-03-28 09:02:59 +0800

    321

---
+++ src/main/java/com/example/demo/Rj1721Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rj1721Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rj1721Application.class, args);
+	}
+
+}
